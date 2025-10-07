24270f323059b5c6d332807dd59a6fb9dedf9e76
Arianna <marym2@andrew.cmu.edu>
2025-10-07 10:08:16 -0400

    change name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,7 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Arianna";
+    return "marym2";
   }
 
   // Match "andrew id", "andrew ID", "Andrew ID", "andrewID", or "andrewid"
